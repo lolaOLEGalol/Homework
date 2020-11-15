e666617272609ba1cd45c1ad5b9c258fafdb69a0
oleg <experementator@gmail.com>
2020-11-15 05:15:25 +0300

    Homework4

---
+++ Class1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class HelpArr
+    {
+        private int Summ = 0; // 2
+        private int maxCount = 0; // 2
+
+        public HelpArr()
+        {
+            
+        } // 1
+
+        public HelpArr(int size, int start, int step)
+        {
+            int [] arr = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                if (i == 0)
+                {
+                    arr[i] = start;
+                }
+                else
+                {
+                    arr[i] = arr[i-1] + step;
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Summ += arr[i];
+            }
+        } // 2
+
+        public int GetSumm()
+        {
+            return Summ;
+        } // 2
+
+        public int GetMaxCount()
+        {
+            return maxCount;
+        } // 2
+
+        public void PrintArr(int[] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine($"{i+1}) {arr[i]}");
+            }
+        } // 1
+
+        public int[] RandomArr(int size, int OT, int DO)
+        {
+            int[] arr = new int[size];
+            Random rnd = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = rnd.Next(OT, DO);
+            }
+            return arr;
+        } // 1
+
+        public int CountCouple(int[] arr, int size)
+        {
+            int k = 0;
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                if (arr[i] % 3 == 0 || arr[i+1] % 3 == 0)
+                {
+                    k++;
+                }
+            }
+            return k;
+        } // 1
+
+        public int[] Invers(int[] arr, int size) // 2
+        {
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = -arr[i];
+            }
+
+            return arr;
+        }
+
+        public int[] Multi(int[] arr, int size, int number) // 2
+        {
+            for(int i = 0; i < size; i++)
+            {
+                arr[i] *= number;
+            }
+
+            return arr;
+        }
+
+        public void MaxCount(int[] arr, int size)
+        {
+            int max = arr[0];
+
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                if (arr[i] == max)
+                {
+                    maxCount++;
+                }
+            }
+
+            
+        } // 2
+
+        public void FileWrite(string vay, int number)
+        {
+            StreamWriter strWrt = new StreamWriter(vay, false, Encoding.ASCII);
+            strWrt.WriteLine(number);
+            strWrt.Flush();
+            strWrt.Close();
+        } // 2B
+
+        public void FileWrite(string vay, int[] arr, int size)
+        {
+            StreamWriter strWrt = new StreamWriter(vay, false, Encoding.ASCII);
+            for (int i = 0; i < size; i++)
+            {
+                strWrt.WriteLine(arr[i]);
+            }
+            
+            strWrt.Flush();
+            strWrt.Close();
+        } // 2B
+
+        public void FileRead(string vay)
+        {
+            if (string.IsNullOrEmpty(vay) || !File.Exists(vay)) return;
+
+            StreamReader strRead = new StreamReader(vay);
+            while (!strRead.EndOfStream)
+            {
+                Console.WriteLine(strRead.ReadLine());
+            }
+            
+            strRead.Close();
+        } // 2B
+        public void Pause()
+        {
+            Console.ReadKey();
+        } // 1
+
+    }
+
+    struct Account
+    {
+        private string login; // 3
+        private string password; // 3
+
+        Account (int SetLoginAndPassword)
+        {
+            login = "start";
+            password = "start";
+        } // 3
+
+        public void SetLoginAndPassword(string loginUser, string passwordUser)
+        {
+            string[] tempArr = new string[2];
+
+            login = loginUser;
+            password = passwordUser;
+
+            tempArr[0] = login;
+            tempArr[1] = password;
+
+            StreamWriter file = new StreamWriter("Account.txt", false, Encoding.ASCII);
+
+            for (int i = 0; i < 2; i++)
+            {
+                file.WriteLine(tempArr[i]);
+            }
+
+            file.Flush();
+            file.Close();
+
+        } // 3
+
+        public bool CheckLoginAndPassword(string loginUser, string passwordUser)
+        {
+            string tempLogin;
+            string tempPassword;
+            StreamReader file = new StreamReader("Account.txt");
+            tempLogin = file.ReadLine();
+            tempPassword = file.ReadLine();
+
+            if (tempLogin == loginUser && tempPassword == passwordUser)
+            {
+                return true;
+            }
+            else
+                return false;
+        } // 3
+
+        public void PrintLoginAndPassword()
+        {
+            Console.WriteLine($"login: {login}");
+            Console.WriteLine($"password: {password}");
+        } // 3
+    }
+
+    class HelpTwoArr
+    {
+        private int[,] arr = new int[10,10];
+        private int minElement;
+        private int maxElement;
+        Random rand = new Random();
+
+        public HelpTwoArr()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                for(int j = 0; j < 10; j++)
+                {
+                    arr[i,j] = rand.Next(0, 100);
+
+                    if (arr[i,j] < minElement)
+                    {
+                        minElement = arr[i, j];
+                    }
+
+                    if (arr[i, j] > maxElement)
+                    {
+                        maxElement = arr[i, j];
+                    }
+                }
+            }
+        }
+
+        public HelpTwoArr(string vay)
+        {
+            StreamWriter file = new StreamWriter(vay, false, Encoding.ASCII);
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    arr[i, j] = rand.Next(0, 100);
+
+                    if (arr[i, j] < minElement)
+                    {
+                        minElement = arr[i, j];
+                    }
+
+                    if (arr[i, j] > maxElement)
+                    {
+                        maxElement = arr[i, j];
+                    }
+
+                    file.WriteLine(arr[i, j]);
+                }
+            }
+            file.Flush();
+            file.Close();
+        } // 4B
+
+        public void ReadArrFILE(string vay)
+        {
+            StreamReader file = new StreamReader(vay);
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    arr[i, j] = Convert.ToInt32(file.ReadLine());
+                }
+            }
+        }
+
+        public int Summ()
+        {
+            int summ = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    summ += arr[i, j];
+                }
+            }
+
+            return summ;
+        }
+
+        public int Summ(int maxNumber)
+        {
+            int summ = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if(arr[i,j] > maxNumber)
+                    {
+                        summ += arr[i, j];
+                    }
+                }
+            }
+
+            return summ;
+        }
+
+        public void returnMaxCount(out int i, out int j)
+        {
+            i = arr[0, 0];
+            j = arr[0, 0];
+
+            for (int i1 = 0; i1 < 10; i++)
+            {
+                for (int j1 = 0; j1 < 10; j++)
+                {
+                    if (arr[i, j] > maxElement)
+                    {
+                        i = i1;
+                        j = j1;
+                    }
+                }
+            }
+        }
+
+        public int GetMaxElement()
+        {
+            return maxElement;
+        }
+
+        public int GetMinElement()
+        {
+            return minElement;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine(arr[i, j]);
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,95 +4,135 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework3
+namespace Homework4
 {
-    class Answer
-    {
-        public void Answer1()
-        {
-            Complex first = new Complex();
-            Complex second = new Complex();
-            Complex result = new Complex();
-
-            first.imaginary = 30;
-            second.imaginary = 4;
-            first.real = 2;
-            second.real = 2;
-
-            Console.WriteLine(string.Format("Answer1\n"));
-
-            result = first.Multi(second);
-            Console.WriteLine($"{result.imaginary} , {result.real} (Multi)\n------------");
-
-            result = first.Division(second);
-            Console.WriteLine($"{result.imaginary} , {result.real} (Division)\n------------");
-
-            result = first.Subtraction(second);
-            Console.WriteLine($"{result.imaginary} , {result.real} (Subtraction)\n------------");
-
-            result = first.Summ(second);
-            Console.WriteLine($"{result.imaginary} , {result.real} (Summ)\n------------");
-        }
-
-        public void Answer2()
-        {
-            Console.WriteLine("\n\nAnswer 2\n");
-            Homework3 test2 = new Homework3();
-            Console.WriteLine($"Пожалуйста, введите числа, чтобы посчитать суммы нечетных положительных чисел. \nДля остановки программы напишите 0.\n");
-            test2.summOddNumber();
-        }
-
-        public void Answer3()
-        {
-            Console.WriteLine("\n\nAnswer 3\n");
-
-            Double test = new Double(), test2 = new Double();
-            
-            Console.WriteLine("Введите числитель и знаменатель 1й дроби:");
-            test.numerator = Convert.ToInt32(Console.ReadLine());
-            test.denominator = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Введите числитель и знаменатель 2й дроби:");
-            test2.numerator = Convert.ToInt32(Console.ReadLine());
-            test2.denominator = Convert.ToInt32(Console.ReadLine());
-
-            if (test.denominator == 0 || test2.denominator == 0)
-            {
-                Console.WriteLine("Вы ввели знаменталь = 0!");
-            }
-            else
-            {
-                Console.WriteLine($"{test.Summ(test2):0.00} - сумма дробей \n{test.Subtraction(test2):0.00} - разность дробей \n{test.Multi(test2):0.00} - перемножение дробей \n{test.Division(test2):0.00} - деление дробей");
-            }
-        }
-
-        public void Answer3Fixed()
-        {
-            DoubleFixed test = new DoubleFixed();
-            DoubleFixed test2 = new DoubleFixed();
-
-            Console.WriteLine("\nВведите числитель и знаменатель 1й дроби:");
-            test.numerator = Convert.ToInt32(Console.ReadLine());
-            test.denominator = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Введите числитель и знаменатель 2й дроби:");
-            test2.numerator = Convert.ToInt32(Console.ReadLine());
-            test2.denominator = Convert.ToInt32(Console.ReadLine());
-
-            test.Summ(test2); test.Subtraction(test2); test.Division(test2); test.Multi(test2);
-        }
-    }
-
     class Program
     {
+        class Answer
+        {
+            public void Answer1()
+            {
+                Console.WriteLine("  Answer 1\n\nСоздать массив из 20 элементов, заполнить от -10000 до 10000 и\nвывести количество пар, где хотя бы 1 элемент делится на 3\n\nИсходный массив:");
+                int size = 20, k;
+                HelpArr test = new HelpArr();
+                int[] arr = new int[size];
+
+                arr = test.RandomArr(size, -10000, 10000);
+                test.PrintArr(arr, size);
+                k = test.CountCouple(arr, size);
+                Console.WriteLine($"\nКоличество пар, где хотя бы 1 элемент делится на 3 = {k}");
+                test.Pause();
+            }
+
+            public void Answer2()
+            {
+                int size = 5;
+                int[] arr = new int[size];
+                HelpArr my = new HelpArr();
+                Console.WriteLine("\n  Answer 2\n");
+
+                Console.WriteLine("\nНачальный массив ---------");
+                arr = my.RandomArr(size, 0, 100);
+                my.PrintArr(arr, size);
+                Console.WriteLine("\nМассив под инверсией -----");
+                arr = my.Invers(arr, size);
+                my.PrintArr(arr, size);
+                Console.WriteLine("\nМассив умноженный на 2 ---");
+                arr = my.Multi(arr, size, 2);
+                my.PrintArr(arr, size);
+                my.MaxCount(arr, size);
+                int k = my.GetMaxCount();
+                Console.WriteLine($"\nКоличество максимальных чисел: {k}");
+
+                Console.ReadKey();
+            }
+
+            public void Answer2B()
+            {
+                HelpArr test = new HelpArr();
+                int[] arr = new int[100];
+
+                for (int i = 0; i < 100; i++)
+                {
+                    arr[i] = i + 1;
+                }
+
+                test.FileWrite("st.cs", 1020);
+                test.FileRead("st.cs");
+
+                Console.ReadKey();
+            }
+
+            public void Answer3()
+            {
+                Account user = new Account();
+
+                string userLogin, userPassword;
+                Console.WriteLine("\n  Answer 3\n\n");
+                Console.WriteLine("Пожалуйста, задайте логин: ");
+                userLogin = Console.ReadLine();
+                Console.WriteLine("Пожалуйста, задайте пароль: ");
+                userPassword = Console.ReadLine();
+
+                user.SetLoginAndPassword(userLogin, userPassword);
+
+                Console.WriteLine("\nОтлично, они сохранились в файле Account.txt :) Теперь попробуйте их ввести!");
+                while(true)
+                {
+                    Console.WriteLine("\nЛогин: ");
+                    userLogin = Console.ReadLine();
+                    Console.WriteLine("Пароль: ");
+                    userPassword = Console.ReadLine();
+
+                    if (user.CheckLoginAndPassword(userLogin, userPassword))
+                    {
+                        Console.WriteLine("\nВы вошли в систему!");
+                        break;
+                    }
+                    else if (userLogin == "1" && userPassword == "1")
+                    {
+                        Console.WriteLine("\nВы досрочно вышли из системы. Удачи!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nЛогин или пароль не верны.Попробуйте еще раз!");
+                        Console.WriteLine("Чтобы выйти досрочно в поле логин и пароль введите 1");
+                    }
+
+                }
+
+                Console.ReadKey();
+            }
+
+            public void Answer4()
+            {
+                Console.WriteLine("\n  Answer 4\nЧтобы увидеть массив раскомментируйте 112 строчку");
+                HelpTwoArr ar = new HelpTwoArr();
+                //ar.Print();
+                Console.WriteLine($" Сумма: {ar.Summ()}");
+                Console.WriteLine($" Сумма от 50: {ar.Summ(50)}");
+                Console.WriteLine($" Максимальный элемент массива: {ar.GetMaxElement()}");
+                Console.WriteLine($" Минимальный элемент массива: {ar.GetMinElement()}");
+
+                HelpTwoArr ar2 = new HelpTwoArr("tee2.txt");
+
+                Console.ReadKey();
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Answer test = new Answer();
+            Answer hw = new Answer();
+            Console.ForegroundColor = ConsoleColor.Cyan;
 
-            test.Answer1();
-            test.Answer2();
-            test.Answer3();
-            test.Answer3Fixed();
+            Console.WriteLine("Чтобы переключиться на следующее задание тыкайте на любую клавишу! :)\n");
+
+            hw.Answer1();
+            hw.Answer2();
+            hw.Answer3();
+            hw.Answer4();
 
             Console.ReadKey();
         }
