32e0fe3a89128dd2d62ff1e932e56f0e65a232ac
oleg <experementator@gmail.com>
2020-11-23 14:25:03 +0300

    Homework5
    Homework5 without numbers 4 and 5

---
+++ Homework5.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace Homework5
+{
+    class Homework
+    {
+        public bool CheckLogin (string userLogin) // 1A
+        {
+            for (int i = 0; i < userLogin.Length; i++)
+            {
+                if (userLogin[i] > 'z' || userLogin[i] < 'a' && !(userLogin[i] >= '0' && userLogin[i] <= '9'))
+                {
+                    return false;
+                }
+            }
+                if ((userLogin[0] >= '0' && userLogin[0] <= '9') || userLogin.Length < 3 || userLogin.Length > 10)
+                {
+                    return false;
+                }
+
+                else
+                    return true;
+        }
+
+        public bool CheckLoginRegular (string userLogin)
+        {
+            string regMask = @"^\D[a-zA-Z0-9]{2,8}$";
+            Regex rg = new Regex(regMask);
+
+            return rg.IsMatch(userLogin);
+        } // 1B
+    }
+
+    static class Message
+    {
+
+        public static void MaxCharInString (string data, int maxChar, StreamWriter file)
+        {
+            string[] arr = data.Split(' ');
+
+            foreach (string s in arr)
+            {
+                if(s.Length < maxChar)
+                {
+                    Console.WriteLine(s);
+                    file.WriteLine(s);
+                }
+            }
+        }
+
+        public static void DeleteWordLastChar (string data, char ch, StreamWriter file)
+        {
+            string[] arr = data.Split(' ');
+            
+            foreach (string s1 in arr)
+            {
+                char simvol = s1[s1.Length - 1];
+                if (simvol != ch)
+                {
+                    Console.WriteLine(s1);
+                    file.WriteLine(s1);
+                }
+            }
+        }
+
+        public static void MaxWord (string data, StreamWriter file)
+        {
+            string[] arr = data.Split(' ');
+            int max = arr[0].Length;
+            string Word = arr[0];
+
+            foreach (string s in arr)
+            {
+                if (s.Length > max)
+                {
+                    Word = s;
+                    max = s.Length;
+                }
+            }
+
+            Console.WriteLine(Word);
+            file.WriteLine(Word);
+        }
+
+        public static void StringFromMaxWords (string data, StreamWriter file)
+        {
+            string[] arr = data.Split(' ');
+            int max = arr[0].Length;
+            string Word = arr[0];
+
+            StringBuilder build = new StringBuilder();
+
+            foreach (string s in arr)
+            {
+                if (s.Length > max)
+                {
+                    max = s.Length;
+                }
+            }
+
+            foreach (string s in arr)
+            {
+                if (s.Length == max)
+                {
+                    build.Append($"{s} ");
+                }
+            }
+
+            Console.WriteLine(build);
+            file.WriteLine(build);
+        }
+    } // 2
+
+    class Permutation
+    {
+        public void String(string text1, string text2)
+        {
+            int a = string.Compare(text1, text2);
+            Console.WriteLine(a);
+        } // 3A
+
+        public void String2(string text1, string text2)
+        {
+            bool check;
+            check = text1.Select(Char.ToUpper).OrderBy(x => x).SequenceEqual(text2.Select(Char.ToUpper).OrderBy(x => x));
+
+            if (check)
+            {
+                Console.WriteLine("Текст является перестановкой другого текста");
+            }
+            else
+            {
+                Console.WriteLine("Тексты отличаются");
+            }
+
+            Console.ReadKey();
+        } //3B
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -3,136 +3,121 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace Homework4
+namespace Homework5
 {
-    class Program
+    class Answer
     {
-        class Answer
+        public void Answer1A()
         {
-            public void Answer1()
-            {
-                Console.WriteLine("  Answer 1\n\nСоздать массив из 20 элементов, заполнить от -10000 до 10000 и\nвывести количество пар, где хотя бы 1 элемент делится на 3\n\nИсходный массив:");
-                int size = 20, k;
-                HelpArr test = new HelpArr();
-                int[] arr = new int[size];
+            Console.WriteLine("\n  Answer1\n");
 
-                arr = test.RandomArr(size, -10000, 10000);
-                test.PrintArr(arr, size);
-                k = test.CountCouple(arr, size);
-                Console.WriteLine($"\nКоличество пар, где хотя бы 1 элемент делится на 3 = {k}");
-                test.Pause();
-            }
+            Homework tw = new Homework();
+            if (tw.CheckLogin("d123"))
+                Console.WriteLine("Ваш логин соответствует требованиям!");
+            else
+                Console.WriteLine("Ваш логин не соответствует требованиям!");
 
-            public void Answer2()
-            {
-                int size = 5;
-                int[] arr = new int[size];
-                HelpArr my = new HelpArr();
-                Console.WriteLine("\n  Answer 2\n");
-
-                Console.WriteLine("\nНачальный массив ---------");
-                arr = my.RandomArr(size, 0, 100);
-                my.PrintArr(arr, size);
-                Console.WriteLine("\nМассив под инверсией -----");
-                arr = my.Invers(arr, size);
-                my.PrintArr(arr, size);
-                Console.WriteLine("\nМассив умноженный на 2 ---");
-                arr = my.Multi(arr, size, 2);
-                my.PrintArr(arr, size);
-                my.MaxCount(arr, size);
-                int k = my.GetMaxCount();
-                Console.WriteLine($"\nКоличество максимальных чисел: {k}");
-
-                Console.ReadKey();
-            }
-
-            public void Answer2B()
-            {
-                HelpArr test = new HelpArr();
-                int[] arr = new int[100];
-
-                for (int i = 0; i < 100; i++)
-                {
-                    arr[i] = i + 1;
-                }
-
-                test.FileWrite("st.cs", 1020);
-                test.FileRead("st.cs");
-
-                Console.ReadKey();
-            }
-
-            public void Answer3()
-            {
-                Account user = new Account();
-
-                string userLogin, userPassword;
-                Console.WriteLine("\n  Answer 3\n\n");
-                Console.WriteLine("Пожалуйста, задайте логин: ");
-                userLogin = Console.ReadLine();
-                Console.WriteLine("Пожалуйста, задайте пароль: ");
-                userPassword = Console.ReadLine();
-
-                user.SetLoginAndPassword(userLogin, userPassword);
-
-                Console.WriteLine("\nОтлично, они сохранились в файле Account.txt :) Теперь попробуйте их ввести!");
-                while(true)
-                {
-                    Console.WriteLine("\nЛогин: ");
-                    userLogin = Console.ReadLine();
-                    Console.WriteLine("Пароль: ");
-                    userPassword = Console.ReadLine();
-
-                    if (user.CheckLoginAndPassword(userLogin, userPassword))
-                    {
-                        Console.WriteLine("\nВы вошли в систему!");
-                        break;
-                    }
-                    else if (userLogin == "1" && userPassword == "1")
-                    {
-                        Console.WriteLine("\nВы досрочно вышли из системы. Удачи!");
-                        break;
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nЛогин или пароль не верны.Попробуйте еще раз!");
-                        Console.WriteLine("Чтобы выйти досрочно в поле логин и пароль введите 1");
-                    }
-
-                }
-
-                Console.ReadKey();
-            }
-
-            public void Answer4()
-            {
-                Console.WriteLine("\n  Answer 4\nЧтобы увидеть массив раскомментируйте 112 строчку");
-                HelpTwoArr ar = new HelpTwoArr();
-                //ar.Print();
-                Console.WriteLine($" Сумма: {ar.Summ()}");
-                Console.WriteLine($" Сумма от 50: {ar.Summ(50)}");
-                Console.WriteLine($" Максимальный элемент массива: {ar.GetMaxElement()}");
-                Console.WriteLine($" Минимальный элемент массива: {ar.GetMinElement()}");
-
-                HelpTwoArr ar2 = new HelpTwoArr("tee2.txt");
-
-                Console.ReadKey();
-            }
+            Console.ReadKey();
         }
 
+        public void Answer1B()
+        {
+            Homework test = new Homework();
 
+            if (test.CheckLoginRegular("dddddddddddfdddf"))
+            {
+                Console.WriteLine("Ваш логин соответствует требованиям!");
+            }
+            else
+            {
+                Console.WriteLine("Ваш логин не соответствует требованиям!");
+            }
+                
+
+            Console.ReadKey();
+        }
+
+        public void Answer2()
+        {
+            StreamWriter file = new StreamWriter("Answer2.txt");
+
+            Console.WriteLine("Привет, сейчас будет тест моих методов :)");
+            file.WriteLine("Привет, сейчас будет тест моих методов :)");
+            Console.WriteLine("Введи предложение, чем длиннее - тем лучше");
+            string data = Console.ReadLine();
+            file.WriteLine($"Твоё предложение: {data}");
+            Console.WriteLine("а) Вывести только те слова сообщения, которые содержат не более n букв.");
+            file.WriteLine("а) Вывести только те слова сообщения, которые содержат не более n букв.");
+            Console.WriteLine("Введи n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            file.WriteLine($"Твоё n: {data}");
+            Console.WriteLine("Ответ: ");
+            file.WriteLine("Ответ: ");
+            Message.MaxCharInString(data, n, file);
+            Console.WriteLine(" ");
+            file.WriteLine(" ");
+
+
+            Console.WriteLine("б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.");
+            file.WriteLine("б) Удалить из сообщения все слова, которые заканчиваются на заданный символ.");
+            Console.WriteLine("Введи символ");
+            char ch = Convert.ToChar(Console.ReadLine());
+            file.WriteLine($"Твой символ: {ch}");
+            Console.WriteLine("Ответ: ");
+            file.WriteLine("Ответ: ");
+            Message.DeleteWordLastChar(data,ch,file);
+            Console.WriteLine(" ");
+            file.WriteLine(" ");
+
+
+            Console.WriteLine("в) Найти самое длинное слово сообщения.");
+            file.WriteLine("в) Найти самое длинное слово сообщения.");
+            Console.WriteLine("Ответ: ");
+            file.WriteLine("Ответ: ");
+            Message.MaxWord(data, file);
+            Console.WriteLine(" ");
+            file.WriteLine(" ");
+
+
+            Console.WriteLine("г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.");
+            file.WriteLine("г) Сформировать строку с помощью StringBuilder из самых длинных слов сообщения.");
+            Console.WriteLine("Ответ: ");
+            file.WriteLine("Ответ: ");
+            Message.StringFromMaxWords(data,file);
+            Console.WriteLine(" ");
+            file.WriteLine(" ");
+
+
+            file.Flush();
+            file.Close();
+        }
+
+        public void Answer3()
+        {
+            Permutation g = new Permutation();
+            string text1, text2;
+            Console.WriteLine("  Answer3\n");
+            Console.WriteLine("Введите 2 текста подряд:");
+            text1 = Console.ReadLine();
+            text2 = Console.ReadLine();
+            g.String(text1, text2);
+        }
+    }
+    class Program
+    {
         static void Main(string[] args)
         {
-            Answer hw = new Answer();
-            Console.ForegroundColor = ConsoleColor.Cyan;
+             Answer test = new Answer();
 
-            Console.WriteLine("Чтобы переключиться на следующее задание тыкайте на любую клавишу! :)\n");
+            //test.Answer1B();
+            //test.Answer2();
 
-            hw.Answer1();
-            hw.Answer2();
-            hw.Answer3();
-            hw.Answer4();
+            //test.Answer3();
+
+            Permutation asd = new Permutation();
+            asd.String("abc", "abc");
 
             Console.ReadKey();
         }
